f40f931871361269a5e8d267875dfe470ca51781
Hoang Hy <hoanghy01122003@gmail.com>
2023-08-18 13:39:39 +0700

    remove criteria temporarily

--- src/app/criteria/page.tsx
+++ src/app/criteria/page.tsx
@@ -1,12 +1,10 @@
-import CriteriaOverallChart from "@/components/Chart/CriteriaOverallChart";
+// import CriteriaOverallChart from "@/components/Chart/CriteriaOverallChart";
 
 export default function CriteriaPage() {
 	return (
 		<div>
 			<h1 className="font-semibold text-3xl">Tiêu chí</h1>
-			<div className="h-[400px]">
-				<CriteriaOverallChart />
-			</div>
+			<div className="h-[400px]">{/* <CriteriaOverallChart /> */}</div>
 		</div>
 	);
 }
