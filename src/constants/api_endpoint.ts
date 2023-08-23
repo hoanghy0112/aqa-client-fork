d15e9b0b597b1f02fcdf74260ac70c314a8e7620
Hoang Hy <hoanghy01122003@gmail.com>
2023-08-24 06:06:29 +0700

    fix: change api endpoint

--- src/constants/api_endpoint.ts
+++ src/constants/api_endpoint.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = "https://aqa-api.hoanghy.tech";
 
-export const GET_SEMESTER_LIST = `${API_BASE_URL}/semester`;
+export const GET_SEMESTER_LIST = `${API_BASE_URL}/semesters`;
 
 export const GET_COMMENT_LIST = `${API_BASE_URL}/comments`;
 export const GET_COMMENT_QUANTITY = `${API_BASE_URL}/comments/size`;
