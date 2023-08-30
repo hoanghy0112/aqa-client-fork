2e853512d314eac7f1f9cbd82d52b3035ce28d81
Hoang Hy <hoanghy01122003@gmail.com>
2023-08-30 22:56:06 +0700

    fix

--- src/components/chart/CriteriaOverallChart.tsx
+++ src/components/chart/CriteriaOverallChart.tsx
@@ -42,7 +42,7 @@ export default function CriteriaOverallChart() {
 					<ProgramSelector />
 					<FacultySelector />
 					<SubjectSelector />
-					<SortSelector defaultValue="" />
+					<SortSelector />
 				</>
 			}
 		>
