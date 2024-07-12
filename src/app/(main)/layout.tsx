3643a81cc6afac0a3d5fb0c805e7167fdbf14704
Hoang Hy <hoanghy01122003@gmail.com>
2024-07-12 16:26:27 +0700

    update

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -32,7 +32,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 	useEffect(() => {
 		if (!loading && !data) {
 			console.log({ data, loading });
-			// router.replace("/signin");
+			router.replace("/signin");
 		}
 	}, [data, loading, router]);
 
