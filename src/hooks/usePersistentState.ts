1f971e290af979aaf5fd04401cd615e0e3cdc0c9
Hoang Hy <hoanghy01122003@gmail.com>
2024-07-21 02:02:55 +0700

    update

--- src/hooks/usePersistentState.ts
+++ src/hooks/usePersistentState.ts
@@ -1,34 +1,18 @@
-import { useState, useEffect, SetStateAction, Dispatch } from "react";
+import {
+	useState,
+	useEffect,
+	SetStateAction,
+	Dispatch,
+	useLayoutEffect,
+} from "react";
 
 export default function usePersistentState<T>(
 	name: string,
-	defaultValue?: T
+	defaultValue: T
 ): [T, Dispatch<SetStateAction<T>>] {
-	const [data, setData] = useState<T>(
-		JSON.parse(localStorage.getItem(name) || JSON.stringify(defaultValue))
-	);
+	const [data, setData] = useState<T>(defaultValue);
 
-	useEffect(() => {
-		const value = localStorage.getItem(name);
-		setData(value != null ? JSON.parse(value) : defaultValue);
-	}, [defaultValue, name]);
-
-	useEffect(() => {
-		if (data) localStorage.setItem(name, JSON.stringify(data));
-	}, [data, name]);
-
-	return [data, setData];
-}
-
-export function usePersistentCookieState<T>(
-	name: string,
-	defaultValue?: T
-): [T, Dispatch<SetStateAction<T>>] {
-	const [data, setData] = useState<T>(
-		JSON.parse(localStorage.getItem(name) || JSON.stringify(defaultValue))
-	);
-
-	useEffect(() => {
+	useLayoutEffect(() => {
 		const value = localStorage.getItem(name);
 		setData(value != null ? JSON.parse(value) : defaultValue);
 	}, [defaultValue, name]);
